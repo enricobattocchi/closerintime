61944ff15f6040f3e8cad133819fa03e1baf7767
Enrico Battocchi <enrico.battocchi@gmail.com>
2019-12-29 02:36:02 +0100

    no source maps

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,12 @@ exports.onCreatePage = async ({ page, actions }) => {
     createPage(page)
   }
 }
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
+  // If production JavaScript and CSS build
+  if (stage === 'build-javascript') {
+    // Turn off source maps
+    actions.setWebpackConfig({
+      devtool: false,
+    })
+  }
+};
